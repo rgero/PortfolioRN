b3edef344837fe2217011b781ee40d7045c58175
Roy Gero <roy.gero.ii@gmail.com>
2024-11-14 22:54:39 -0500

    Adding Linking and the stub for the Project Details Screen

--- Portfolio/App.js
+++ Portfolio/App.js
@@ -1,27 +1,64 @@
 import { MD3DarkTheme, PaperProvider, useTheme } from 'react-native-paper';
+import React, { useEffect, useState } from 'react';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import CustomDrawer from './src/drawer/CustomDrawer';
 import HomeScreen from './src/screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import NotificationsScreen from './src/screens/NotificationsScreen';
+import ProjectDetailsScreen from './src/screens/ProjectDetailsScreen';
 import ProjectListScreen from './src/screens/ProjectListScreen';
-import React from 'react';
 import ResumeScreen from './src/screens/ResumeScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { useColorScheme } from 'react-native-web';
 
 const Drawer = createDrawerNavigator();
 
+const linking = {
+  prefixes: ['roymondnet://', 'https://www.roymond.net'],
+  config: {
+    screens: {
+      Home: '',
+      Notifications: 'notifications',
+      'Project List': 'projects',
+      'Project Details': 'projects/:id',
+      Resume: 'resume',
+    },
+  },
+};
+
 export default function App() {
   const colorScheme = useColorScheme();
   const theme = colorScheme === 'dark' ? MD3DarkTheme : MD3LightTheme;
 
+  const [initialState, setInitialState] = useState();
+
+  // Load the saved state on initial app load
+  useEffect(() => {
+    const loadNavigationState = async () => {
+      const savedState = await AsyncStorage.getItem('NAVIGATION_STATE');
+      if (savedState) {
+        setInitialState(JSON.parse(savedState));
+      }
+    };
+    loadNavigationState();
+  }, []);
+
+  // Save the navigation state on every change
+  const onStateChange = async (state) => {
+    await AsyncStorage.setItem('NAVIGATION_STATE', JSON.stringify(state));
+  };
+
+
   return (
     <PaperProvider theme={theme}>
-      <NavigationContainer>
+      <NavigationContainer
+        linking={linking}
+        initialState={initialState}
+        onStateChange={onStateChange}
+      >
         <Drawer.Navigator 
           drawerContent={(props) => <CustomDrawer {...props}/> }
-          initialRouteName="Home"
           screenOptions={({ route }) => {
             const { colors } = useTheme();
             return {
@@ -41,6 +78,7 @@ export default function App() {
           <Drawer.Screen name="Home" component={HomeScreen} />
           <Drawer.Screen name="Notifications" component={NotificationsScreen} />
           <Drawer.Screen name="Project List" component={ProjectListScreen} />
+          <Drawer.Screen name="Project Details" component={ProjectDetailsScreen} />
           <Drawer.Screen name="Resume" component={ResumeScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
--- Portfolio/src/screens/ProjectDetailsScreen.js
+++ Portfolio/src/screens/ProjectDetailsScreen.js
@@ -1,9 +1,10 @@
 import { Surface, Text } from "react-native-paper";
 
-const ProjectDetailsScreen = () => {
+const ProjectDetailsScreen = ({route}) => {
   return (
     <Surface style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text variant="displayLarge">Project Detail Screen</Text>
+      <Text variant="displayMedium">{route.params.id}</Text>
     </Surface>
   );
 }
