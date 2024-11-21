c5eea17f0024e597d139ea60272a0d4f9d7b427d
Roy Gero <roy.gero.ii@gmail.com>
2024-11-20 22:10:57 -0500

    Adding a quick resume page that'll work on both Web and with React Native.

--- Portfolio/src/screens/ResumeScreen.js
+++ Portfolio/src/screens/ResumeScreen.js
@@ -1,9 +1,31 @@
-import { Surface, Text } from "react-native-paper";
+import { Platform } from 'react-native';
+import { SUPABASE_BUCKET_URL } from '@env';
+import { Surface } from "react-native-paper";
+import { View } from 'react-native-web';
+import WebView from 'react-native-webview';
 
 const ResumeScreen = () => {
+
+  const isWeb = Platform.OS === 'web';
+  const pdfUrl = `${SUPABASE_BUCKET_URL}/files/GeroResume.pdf#navpanes=0`;
+
   return (
     <Surface style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text variant="displayLarge">Resume</Text>
+      {isWeb ? (
+        // If on web, use an iframe instead of WebView
+        <iframe 
+          src={pdfUrl} 
+          width="100%" 
+          height="100%" 
+          title="Gero's Resume"
+          style={{ border: 'none' }}
+        />
+      ) : (
+        <WebView
+          source={{ uri: pdfUrl }}
+          style={{ flex: 1 }}
+        />
+      )}
     </Surface>
   );
 }
