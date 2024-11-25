fb7b035ed38012cf0511fe4ba1dbab34700e3aa8
Roy Gero <roy.gero.ii@gmail.com>
2024-11-25 16:05:07 -0500

    Changing the way the drawer looks so the icons are at the bottom.

--- Portfolio/src/drawer/CustomDrawer.js
+++ Portfolio/src/drawer/CustomDrawer.js
@@ -1,40 +1,48 @@
 import { Drawer, useTheme } from "react-native-paper";
-import { DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
 
+import { DrawerContentScrollView } from "@react-navigation/drawer";
 import DrawerHeader from "./DrawerHeader";
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import SocialContainer from "./SocialContainer";
+import { View } from 'react-native';
 
 const CustomDrawer = (props) => {
   const theme = useTheme();
 
   return (
-    <DrawerContentScrollView {...props} style={{ backgroundColor: theme.colors.background }}>
-      <DrawerHeader/>
-      <Drawer.Item
-        label="Home"
-        onPress={() => props.navigation.navigate('Home')}
-        icon={() => <MaterialIcons name="home" size={24} color={theme.colors.primary} />}
-      />
-      <Drawer.Item
-        label="Notifications"
-        onPress={() => props.navigation.navigate('Notifications')}
-        icon={() => <MaterialIcons name="notifications" size={24} color={theme.colors.primary} />}
-      />
-      <Drawer.Item
-        label="Project List"
-        onPress={() => props.navigation.navigate('Projects')}
-        icon={() => <MaterialIcons name="list" size={24} color={theme.colors.primary} />}
-      />
-      <Drawer.Item
-        label="Resume"
-        onPress={() => props.navigation.navigate('Resume')}
-        icon={() => <MaterialIcons name="description" size={24} color={theme.colors.primary} />}
-      />
+    <View style={{
+      flex: 1,
+      paddingBottom: 20,
+      backgroundColor: theme.colors.background
+    }}>
+      <DrawerContentScrollView {...props} style={{ backgroundColor: theme.colors.background }}>
+        <View>
+          <DrawerHeader/>
+          <Drawer.Item
+            label="Home"
+            onPress={() => props.navigation.navigate('Home')}
+            icon={() => <MaterialIcons name="home" size={24} color={theme.colors.primary} />}
+          />
+          <Drawer.Item
+            label="Notifications"
+            onPress={() => props.navigation.navigate('Notifications')}
+            icon={() => <MaterialIcons name="notifications" size={24} color={theme.colors.primary} />}
+          />
+          <Drawer.Item
+            label="Project List"
+            onPress={() => props.navigation.navigate('Projects')}
+            icon={() => <MaterialIcons name="list" size={24} color={theme.colors.primary} />}
+          />
+          <Drawer.Item
+            label="Resume"
+            onPress={() => props.navigation.navigate('Resume')}
+            icon={() => <MaterialIcons name="description" size={24} color={theme.colors.primary} />}
+          />
+        </View>
+      </DrawerContentScrollView>
       <SocialContainer/>
-    </DrawerContentScrollView>
+    </View>
   );
 }
 
-
-export default CustomDrawer
+export default CustomDrawer;
--- Portfolio/src/drawer/SocialContainer.js
+++ Portfolio/src/drawer/SocialContainer.js
@@ -42,7 +42,8 @@ const styles = StyleSheet.create({
   container: {
     borderRadius: 8,
     elevation: 4,
-    marginTop: 30
+    marginTop: 30,
+    marginHorizontal: 10,
   },
   iconContainer: {
     flexDirection: 'row',
