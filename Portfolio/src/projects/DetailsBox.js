086bb83f7c8db6092a6154f461b25e7e1762a0eb
Roy Gero <roy.gero.ii@gmail.com>
2024-11-30 18:27:15 -0500

    Making the details box smaller.

--- Portfolio/src/projects/DetailsBox.js
+++ Portfolio/src/projects/DetailsBox.js
@@ -1,16 +1,22 @@
 import { Button, Card, Text } from "react-native-paper"
 
+import { Dimensions } from "react-native";
 import { View } from "react-native-web"
 import { openLink } from '../utils/openLink';
 
 const DetailsBox = ({details}) => {
+  const { width: screenWidth } = Dimensions.get('window');
 
   // If nothing is passed, return null
   if (details.website === "" && details.repo === "") return null
-
-
+  
   return (
-    <Card style={{width: "40%", alignSelf: "center"}}>
+    <Card style={
+      {
+        width: screenWidth < 768 ? '75%' : '20%',
+        alignSelf: "center"
+      }
+    }>
       <Card.Content>
         <View style={{flex: 1, flexDirection: 'column', justifyContent: "flex-end"}}>
           <Text variant="bodyLarge">Links</Text>
