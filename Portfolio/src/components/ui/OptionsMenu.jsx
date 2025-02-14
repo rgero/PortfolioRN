a00f2ebb610dbd0dc83fae9d1ae3f42e9dd62112
Roy Gero <roy.gero.ii@gmail.com>
2025-02-14 15:24:34 -0500

    #3 Adjusting the height of the Menu Icon

--- Portfolio/src/components/ui/OptionsMenu.jsx
+++ Portfolio/src/components/ui/OptionsMenu.jsx
@@ -4,7 +4,6 @@ import { Menu, Searchbar, IconButton } from "react-native-paper";
 import { isMobile } from "../../utils/isMobile";
 import { RFPercentage } from "react-native-responsive-fontsize";
 import { useSearchContext } from "../../context/SearchContext";
-import { KeyboardAvoidingView } from "react-native-web";
 
 const OptionsMenu = () => {
   const [visible, setVisible] = useState(false);
@@ -14,26 +13,24 @@ const OptionsMenu = () => {
   const closeMenu = () => setVisible(false);
 
   return (
-    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
-      <View style={{ flexDirection: "row", justifyContent: "center", marginTop: 20 }}>
-        <Menu
-          visible={visible}
-          onDismiss={closeMenu}
-          anchor={
-            <IconButton icon="magnify" onPress={openMenu} size={isMobile() ? RFPercentage(3) : RFPercentage(2)}/>
-          }
-          anchorPosition="bottom"
-          contentStyle={{ width: 300, padding: "10px" }} // Controls menu width
-        >
-          <Searchbar
-            placeholder="Search..."
-            value={searchText}
-            onChangeText={handleSearchChange}
-            style={{ margin: 10, width: "95%" }}
-          />
-        </Menu>
-      </View>
-    </KeyboardAvoidingView>
+    <View style={{ flexDirection: "row", justifyContent: "center" }}>
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={
+          <IconButton icon="magnify" onPress={openMenu} size={isMobile() ? RFPercentage(3) : RFPercentage(2)}/>
+        }
+        anchorPosition="bottom"
+        contentStyle={{ width: 300, padding: "10px" }} // Controls menu width
+      >
+        <Searchbar
+          placeholder="Search..."
+          value={searchText}
+          onChangeText={handleSearchChange}
+          style={{ margin: 10, width: "95%" }}
+        />
+      </Menu>
+    </View>
   );
 };
 
