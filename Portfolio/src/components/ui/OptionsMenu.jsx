b7caed6b4b73139b9987b533e732cfd50273c9eb
Roy Gero <roy.gero.ii@gmail.com>
2025-02-14 15:01:35 -0500

    #3 - Adding the ability to search projects

---
+++ Portfolio/src/components/ui/OptionsMenu.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Menu, Searchbar, IconButton } from "react-native-paper";
+import { isMobile } from "../../utils/isMobile";
+import { RFPercentage } from "react-native-responsive-fontsize";
+import { useSearchContext } from "../../context/SearchContext";
+
+const SearchMenu = () => {
+  const [visible, setVisible] = useState(false);
+  const {searchText, handleSearchChange} = useSearchContext();
+
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "center", marginTop: 20 }}>
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={
+          <IconButton icon="magnify" onPress={openMenu} size={isMobile() ? RFPercentage(3) : RFPercentage(2)}/>
+        }
+        anchorPosition="bottom"
+        contentStyle={{ width: 300, padding: "10px" }} // Controls menu width
+      >
+        <Searchbar
+          placeholder="Search..."
+          value={searchText}
+          onChangeText={handleSearchChange}
+          style={{ margin: 10, width: "95%" }}
+        />
+      </Menu>
+    </View>
+  );
+};
+
+export default SearchMenu;
--- Portfolio/src/screens/ProjectListScreen.jsx
+++ Portfolio/src/screens/ProjectListScreen.jsx
@@ -2,35 +2,18 @@ import PageLayout from "../components/ui/PageLayout";
 import ProjectList from "../components/projects/ProjectList";
 import Title from "../components/ui/Title";
 import { View } from "react-native-web";
-import { IconButton } from "react-native-paper";
-import { RFPercentage } from "react-native-responsive-fontsize";
-import { isMobile } from "../utils/isMobile";
+import OptionsMenu from "../components/ui/OptionsMenu";
 
 const ProjectListScreen = () => {
   return (
     <PageLayout>
-      <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
         <Title>Projects</Title>
-        <View style={{flexDirection: "row", spacing: "5px", alignItems: "center"}}>
-          {searchText.length > 0 && (
-            <IconButton
-              icon="close"
-              size={isMobile() ? RFPercentage(3) : RFPercentage(2)}
-              onPress={() => clearSearch(true)}
-            />
-          )}
-          <IconButton
-            icon="magnify"
-            size={isMobile() ? RFPercentage(3) : RFPercentage(2)}
-            onPress={() => setIsOpen(true)}
-          />
-        </View>
-
+        <OptionsMenu/>
       </View>
-
-      <ProjectList/>
+      <ProjectList />
     </PageLayout>
   );
-}
+};
 
-export default ProjectListScreen
+export default ProjectListScreen;
