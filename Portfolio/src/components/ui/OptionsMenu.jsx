ebd06825eeb6783c2127c6ddd6addbe2774bef10
Roy Gero <roy.gero.ii@gmail.com>
2025-02-15 14:43:12 -0500

    #3, trying to stop propagation to keep the menu open

--- Portfolio/src/components/ui/OptionsMenu.jsx
+++ Portfolio/src/components/ui/OptionsMenu.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, Platform } from "react-native";
 import { Menu, Searchbar, IconButton } from "react-native-paper";
 import { isMobile } from "../../utils/isMobile";
 import { RFPercentage } from "react-native-responsive-fontsize";
@@ -7,7 +7,7 @@ import { useSearchContext } from "../../context/SearchContext";
 
 const OptionsMenu = () => {
   const [visible, setVisible] = useState(false);
-  const {searchText, handleSearchChange} = useSearchContext();
+  const { searchText, handleSearchChange } = useSearchContext();
 
   const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
@@ -18,7 +18,11 @@ const OptionsMenu = () => {
         visible={visible}
         onDismiss={closeMenu}
         anchor={
-          <IconButton icon="magnify" onPress={openMenu} size={isMobile() ? RFPercentage(3) : RFPercentage(2)}/>
+          <IconButton
+            icon="magnify"
+            onPress={openMenu}
+            size={isMobile() ? RFPercentage(3) : RFPercentage(2)}
+          />
         }
         anchorPosition="bottom"
         contentStyle={{ width: 300, padding: "10px" }} // Controls menu width
@@ -27,9 +31,11 @@ const OptionsMenu = () => {
           placeholder="Search..."
           value={searchText}
           onChangeText={handleSearchChange}
-          onFocus={openMenu}
-          onBlur={()=> setTimeout(closeMenu, 200)}
+          onFocus={openMenu} // Keep menu open when focusing
           style={{ margin: 10, width: "95%" }}
+          {...(Platform.OS === "web" && {
+            onMouseDown: (e) => e.stopPropagation(), // Prevent menu dismissal on web
+          })}
         />
       </Menu>
     </View>
