e0de711df55b7786afbd918e022244e18ca7749b
Roy Gero <roy.gero.ii@gmail.com>
2025-11-07 15:06:54 -0500

    Removing the unused hook.

--- Portfolio/src/components/projects/hooks/useGetProjects.js
+++
@@ -1,7 +0,0 @@
-import { getProjects } from "../../../services/apiProjects";
-import { useQuery } from "@tanstack/react-query";
-
-export const useGetProjects = () => {
-  const {isLoading, data: projects = [], error, refetch} = useQuery({queryKey: ["projects"], queryFn: ()=> getProjects()});
-  return { isLoading, projects, error, refetch};
-}
